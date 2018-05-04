bc09b2d4502c285cc0697eb14034b70d83706e0f
David Julakidze <datoj81@gmail.com>
2018-05-04 00:41:52 -0400

    added urlgetter method for different keys

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import urllib.parse
 
 key = 0 # The api key
 action = 0 # action that you performing most of the time feeds
-type = "json"
 
-url = "http://api.broadcastify.com/audio/?"+ action +"=feeds&type="+ type +"&key=" + key
+
+def urlgetter(action,key):
+    url = "http://api.broadcastify.com/audio/?"+ action +"=feeds&type=type&key=" + key
+
+    contents = urllib.request.urlopen(url).read()
+    return contents
+
+urlgetter(action,key)
 
